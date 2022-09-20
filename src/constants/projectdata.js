51a80a15f24fb325a7b1f7b8a95f4411dfb2977b
Juan H <juantwothreeinseries@gmail.com>
2022-09-19 20:59:08 -0500

    added last project image

--- src/constants/projectdata.js
+++ src/constants/projectdata.js
@@ -1,5 +1,6 @@
 import Chatter from '../assets/Chatter.png';
 import QuickSnags from '../assets/QuickSnags.png';
+import SpotifyMonthly from '../assets/SpotifyMonthly.png';
 
 const projectData = [
 	{
@@ -8,7 +9,7 @@ const projectData = [
 		repoUrl         : 'https://github.com/JJHDZ123/QuickSnags-MERN',
 		projectImageUrl : QuickSnags,
 		description     :
-			'Full feature commerce site, for all your favorite snacks and interesting snacks from aroudn the world. Please Give it a little time for the web Dynos to spin up as it is hosted on Heroku'
+			'Full feature commerce site, for all your favorite snacks and interesting snacks from aroudn the world. Please give it a little time for the web Dynos to spin up as it is hosted on Heroku'
 	},
 	{
 		name            : 'Chatter',
@@ -16,15 +17,15 @@ const projectData = [
 		repoUrl         : 'https://github.com/JJHDZ123/Chatter-MERN',
 		projectImageUrl : Chatter,
 		description     :
-			'Real-time Chat app that utilizes Websockets and fully fledged out user-sign in with persistant sign-in. Please Give it a little time for the web Dynos to spin up as it is hosted on Heroku'
+			'Real-time Chat app that utilizes Websockets and fully fledged out user-sign in with persistant sign-in. Please give it a little time for the web Dynos to spin up as it is hosted on Heroku'
 	},
 	{
 		name            : 'Spotify Monthly',
 		liveUrl         : 'https://spotify-monthly-1.herokuapp.com/',
 		repoUrl         : 'https://github.com/JJHDZ123/Spotify-Monthly',
-		projectImageUrl : '',
+		projectImageUrl : SpotifyMonthly,
 		description     :
-			'Live aplication used to make monthly spotify playlists for all your favorite artists. Please Give it a little time for the web Dynos to spin up as it is hosted on Heroku'
+			'Live application used to make monthly spotify playlists for all your favorite artists. Please give it a little time for the web Dynos to spin up as it is hosted on Heroku'
 	}
 ];
 
